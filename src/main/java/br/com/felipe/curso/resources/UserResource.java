c9e76f4befb1fa1d3b6fdefa2bf3f6320fd2a65d
Felipe Roque <feliperamosroque@gmail.com>
2020-06-12 16:11:45 -0300

    User entity and resource

---
+++ src/main/java/br/com/felipe/curso/resources/UserResource.java
@@ -0,0 +1,21 @@
+package br.com.felipe.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.felipe.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Felipe", "felipe@gmail.com", "999999999", "12345");
+		
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
