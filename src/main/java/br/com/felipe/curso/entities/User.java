8adc46bad0bdbea0da328c8686e585a7945a867b
Felipe Roque <feliperamosroque@gmail.com>
2020-06-12 19:26:21 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/felipe/curso/entities/User.java
+++ src/main/java/br/com/felipe/curso/entities/User.java
@@ -2,11 +2,19 @@ package br.com.felipe.curso.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -16,7 +24,7 @@ public class User implements Serializable{
 
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
