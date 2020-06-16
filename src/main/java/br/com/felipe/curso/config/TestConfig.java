739cc67a8188c136855e455180cbc1bc68705887
Felipe Roque <feliperamosroque@gmail.com>
2020-06-16 14:35:47 -0300

    Payment one-to-one association

--- src/main/java/br/com/felipe/curso/config/TestConfig.java
+++ src/main/java/br/com/felipe/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.felipe.curso.entities.Category;
 import br.com.felipe.curso.entities.Order;
 import br.com.felipe.curso.entities.OrderItem;
+import br.com.felipe.curso.entities.Payment;
 import br.com.felipe.curso.entities.Product;
 import br.com.felipe.curso.entities.User;
 import br.com.felipe.curso.entities.enums.OrderStatus;
@@ -46,12 +47,6 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1);
-
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-
 		Category cat1 = new Category(null, "Eletronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
@@ -74,12 +69,22 @@ public class TestConfig implements CommandLineRunner {
 
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1);
+
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 
 	}
 }
